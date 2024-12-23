f9c991ad6a281e6db7fd3118167da46b59839231
bu-sy <bartlomiej.pawel.zak@gmail.com>
2024-12-23 15:08:08 +0100

    Expanded Points and Loader -> introduced basic logic and tests

--- loader.py
+++ loader.py
@@ -1,3 +1,34 @@
+from points import Point
+import yaml
+
 class Loader(object):
-    def __init__(self, filePath):
-        pass
+    def __init__(self, yamlDefinition):
+        self._startPoints = []
+        self._board = None
+
+        definition = yaml.safe_load(yamlDefinition)['definition']
+        for pointDefinition in definition['distance_points']:
+            assert len(pointDefinition) == 2
+            self._startPoints.append(
+                Point(*pointDefinition)
+            )
+
+    def getStartPoints(self):
+        return self._startPoints
+
+
+if __name__ == "__main__":
+    print("TEST")
+    loader = Loader('''
+    definition:
+      board:
+        mesh_resolution: 0.01
+        square:
+          xrange: [0, 1]
+          yrange: [0, 1]
+      distance_points:
+        - [0.5, 0.5]
+    ''')
+    assert len(loader.getStartPoints()) == 1
+    assert loader.getStartPoints()[0].equals(Point(0.5, 0.5))
+    print("TEST SUCCEEDED")
--- points.py
+++ points.py
@@ -7,3 +7,18 @@ class Point(object):
 
     def distance_from(self, point):
         return math.sqrt((point.x-self.x) ** 2 + (point.y-self.y) ** 2)
+
+    def equals(self, point):
+        return (self.x == point.x) and (self.y == point.y)
+
+
+if __name__ == "__main__":
+    print("TEST")
+    print("TEST EQUALS")
+    assert Point(1, 2).equals(Point(1, 2))
+    assert not Point(1, 2).equals(Point(2, 1))
+    print("TEST DISTANCE")
+    assert Point(0, 3).distance_from(Point(4, 0)) == 5
+    assert Point(0, 3).distance_from(Point(0, 5)) == 2
+    assert Point(7, 7).distance_from(Point(7, 7)) == 0
+    print("TEST SUCCEEDED")
