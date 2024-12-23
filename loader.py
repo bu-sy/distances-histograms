0b0fa8524d20a0f90b7db6e03f0d65d5c5a7f094
bu-sy <bartlomiej.pawel.zak@gmail.com>
2024-12-23 19:04:29 +0100

    Added Line

--- loader.py
+++ loader.py
@@ -1,12 +1,23 @@
-from points import Point, Grid
+from points import Point, Line, Grid
 import yaml
+
 
 class Loader(object):
     def __init__(self, yamlDefinition):
         self._grid = None
-        self._board = None
-
         definition = yaml.safe_load(yamlDefinition)['definition']
+        self._pixelResolution = definition['board']['pixel_resolution']
+        xrange = definition['board']['square']['xrange']
+        yrange = definition['board']['square']['yrange']
+        self._firstSide = Line(
+            Point(xrange[0], yrange[0]),
+            Point(xrange[0], yrange[1])
+        )
+        self._secondSide = Line(
+            Point(xrange[0], yrange[0]),
+            Point(xrange[1], yrange[0])
+        )
+
         points = []
         for pointDefinition in definition['distance_points']:
             assert len(pointDefinition) == 2
@@ -18,13 +29,18 @@ class Loader(object):
     def getGrid(self):
         return self._grid
 
+    def getMesh(self):
+        for firstPoint in self._firstSide.getDivide(self._pixelResolution):
+            for secondPoint in self._secondSide.getDivide(self._pixelResolution):
+                yield firstPoint.combine(secondPoint)
+
 
 if __name__ == "__main__":
     print("TEST")
     loader = Loader('''
     definition:
       board:
-        mesh_resolution: 0.01
+        pixel_resolution: 5
         square:
           xrange: [0, 1]
           yrange: [0, 1]
@@ -33,4 +49,8 @@ if __name__ == "__main__":
     ''')
     assert len(loader.getGrid().points) == 1
     assert loader.getGrid().points[0].equals(Point(0.5, 0.5))
+    print("TEST MESH")
+    mesh = list(loader.getMesh())
+    assert mesh[8].equals(Point(0.75, 0.25))
+    assert mesh[12].equals(Point(0.5, 0.5))
     print("TEST SUCCEEDED")
--- points.py
+++ points.py
@@ -12,6 +12,37 @@ class Point(object):
     def equals(self, point):
         return (self.x == point.x) and (self.y == point.y)
 
+    def combine(self, point):
+        return Point(
+            self.x + point.x,
+            self.y + point.y,
+        )
+
+    def __str__(self):
+        return f"POINT({self.x, self.y})"
+
+
+class Line(object):
+    def __init__(self, pointStart, pointEnd):
+        self.startPoint = pointStart
+        self.endPoint = pointEnd
+
+    def getDivide(self, point_number):
+        def getWeightedValue(first, second, percent):
+            return second * percent + (1-percent) * first
+
+        assert point_number >= 2
+        for a in range(0, point_number):
+            percentage = a / (point_number-1)
+            yield Point(
+                getWeightedValue(self.startPoint.x, self.endPoint.x, percentage),
+                getWeightedValue(self.startPoint.y, self.endPoint.y, percentage)
+            )
+
+    def __str__(self):
+        return f"LINE({self.startPoint}; {self.endPoint})"
+
+
 class Grid(object):
     def __init__(self, points):
         self.points = points
@@ -28,6 +59,8 @@ if __name__ == "__main__":
     assert Point(0, 3).distance_from(Point(4, 0)) == 5
     assert Point(0, 3).distance_from(Point(0, 5)) == 2
     assert Point(7, 7).distance_from(Point(7, 7)) == 0
+    print("TEST POINT ADD")
+    assert Point(3, 7).combine(Point(4, 5)).equals(Point(7, 12))
     print("TEST GRID DISTANCE")
     grid = Grid([
         Point(0, 0),
@@ -37,4 +70,13 @@ if __name__ == "__main__":
     assert grid.distance_from(Point(2, 2)) == 2
     assert grid.distance_from(Point(0, 1)) == 1
     assert grid.distance_from(Point(0, 1.5)) == 0.5
-    print("TEST SUCCEEDED")
+    print("TESTS LINE DIVIDE")
+    line = Line(
+        Point(0, 0),
+        Point(10, 20)
+    )
+    pointsOnLine = list(line.getDivide(3))
+    assert pointsOnLine[0].equals(Point(0, 0))
+    assert pointsOnLine[1].equals(Point(5, 10))
+    assert pointsOnLine[2].equals(Point(10, 20))
+    print("TESTS SUCCEEDED")
